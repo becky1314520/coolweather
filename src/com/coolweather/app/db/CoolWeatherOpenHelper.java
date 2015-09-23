1023c5bdd84d07ad4895645cca7ff435ab35b652
becky1314520 <86157791@qq.com>
2015-09-23 22:25:20 +0800

    test

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -8,26 +8,28 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	
 	/**
-	 *  Privince表表建表语句
+	 *  Province表表建表语句
 	 */
-	public static final String CREATE_PROVICE = "create table Privance ("
+	public static final String CREATE_PROVICE = "create table Province ("
 			+ "id integer primary key autoincrement, "
-			+ "privince_name text, "
-			+ "privince_code text) ";
+			+ "province_name text, "
+			+ "province_code text) ";
 	/**
 	 * City表建表语句
 	 */
 	public static final String CREATE_CITY = "create table City ("
 			+ "id integer primary key autoincrement, "
 			+ "city_name text, "
-			+ "city_code text) ";
+			+ "city_code text, "
+			+ "province_id integer)";
 	/**
 	 *  County表建表语句
 	 */
 	public static final String CREATE_COUNTY = "create table County ("
 			+ "id integer primary key autoincrement, "
 			+ "county_name text, "
-			+ "county_code text) ";
+			+ "county_code text, "
+			+ "city_id integer)";
 	
 	public CoolWeatherOpenHelper (Context context, String name, CursorFactory factory, int version) {
 		super(context, name, factory, version);
@@ -42,7 +44,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		
+
 	}
 	
 }
